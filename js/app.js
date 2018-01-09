e37e1de832dff050bfa444f96d0f2edc29ed90e5
Hyder Bangash <hyder.bangash@stellatelecom.com>
2018-01-09 18:02:49 +0400

    remove commented code

--- js/app.js
+++ js/app.js
@@ -50,33 +50,3 @@ function renderStatus (statusText) {
   $('#status').html(statusText);
 
 }
-
-// function getCurrentTabUrl (callback) {
-//   // Query filter to be passed to chrome.tabs.query - see
-//   // https://developer.chrome.com/extensions/tabs#method-query
-//   var queryInfo = {
-//     active: true,
-//     currentWindow: true
-//   };
-//
-//   chrome.tabs.query(queryInfo, function (tabs) {
-//
-//     var tab = tabs[0];
-//
-//     var url = tab.url;
-//
-//     callback(url);
-//   });
-// }
-
-// $(document).ready(function () {
-//
-//   getCurrentTabUrl(function (url) {
-//
-//     // Put the image URL in Google search.
-//     renderStatus('Analyzing ' + url);
-//
-//   });
-// });
-
-
